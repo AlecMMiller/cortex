b6e4e3d85fe7ac53ee0101f791804252ae0f79f2
Alec Miller <alecmichaelmiller@gmail.com>
2024-06-27 18:34:35 -0700

    basic lexical editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import "./App.css";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {
+  // Theme styling goes here
+  //...
+}
+
+function onError(error: any) {
+  console.error(error);
+}
+
+function Editor() {
+  const initialConfig = {
+    namespace: 'MyEditor',
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <RichTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <AutoFocusPlugin />
+    </LexicalComposer>
+  );
+}
 
 function App() {
   return (
     <div className="container">
-      Testing
+      <Editor />
     </div>
   );
 }
