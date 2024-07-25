06eefa6dd5bfa94c5e54cd2601526ed087f81834
Alec Miller <alecmichaelmiller@gmail.com>
2024-07-24 20:32:20 -0700

    bundled executable works with database

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::env;
+use std::fs::create_dir_all;
 
+use tauri_plugin_fs::FsExt;
 use lexical::EditorState;
 use serde_json::Error;
-use sqlx::{migrate::MigrateDatabase, Pool, Sqlite, SqlitePool};
-use tauri::State;
+use sqlx::{sqlite::SqliteConnectOptions, SqlitePool};
+use tauri::{Manager, State};
 
 mod lexical;
 mod notes;
@@ -54,26 +55,25 @@ async fn create_note<'a>(name: &str, pool: State<'_, SqlitePool>) -> Result<Stri
     }
 }
 
-#[tokio::main]
-async fn main() {
-    //let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let db_url = "sqlite:test.db";
-
-    if !sqlx::Sqlite::database_exists(&db_url)
-        .await
-        .expect("Failed to check if database exists")
-    {
-        sqlx::Sqlite::create_database(&db_url)
-            .await
-            .expect("Failed to create database");
-    }
-
-    let pool = SqlitePool::connect("sqlite:test.db").await.unwrap();
-
-    sqlx::migrate!().run(&pool).await.unwrap();
-
+fn main() {
     tauri::Builder::default()
-        .manage(pool)
+        .plugin(tauri_plugin_fs::init())
+        .setup(|app| {
+            let path = app.path().app_data_dir().expect("This should never be None");
+  
+            create_dir_all(&path).unwrap();
+            let db_path = path.join("data.db");
+            let runtime = tokio::runtime::Runtime::new().expect("Unable to create a runtime");
+            let pool = runtime.block_on(async {
+                let conn = SqliteConnectOptions::new().filename(db_path).create_if_missing(true);
+                let pool = SqlitePool::connect_with(conn).await.unwrap();
+                sqlx::migrate!().run(&pool).await.unwrap();
+                pool
+            });
+
+            app.manage(pool);
+            Ok(())
+         })
         .invoke_handler(tauri::generate_handler![create_note, editor_change_state, get_last_updated])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
