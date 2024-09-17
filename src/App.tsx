fb8e5758b82e1ef9faa06c895d4a08466c30ff5e
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-17 12:33:52 -0700

    components for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,33 @@ import { useEffect, useState } from "react";
 import { getLastUpdated } from "./commands/note";
 import Editor from './editor';
 import { NoteData } from "./types";
-import { Maximize, Minimize2, Search, Settings, X } from "lucide-react";
+import { Maximize, Minimize2, Search, Settings, X, FilePlus2, Icon } from "lucide-react";
 import { getCurrentWindow } from '@tauri-apps/api/window';
+
+import {
+  Tooltip, TooltipContent, TooltipTrigger, TooltipProvider
+} from "@/components/ui/tooltip"
+
+interface SideButtonProps {
+  icon: Icon
+  tooltip: string
+};
+
+function SideButton(props: SideButtonProps): JSX.Element {
+  let Actual = props.icon;
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>
+          <Actual className="m-2 text-subtext1 hover:text-text" size={24}/>
+        </TooltipTrigger>
+        <TooltipContent side="right">
+          {props.tooltip}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  )
+};
 
 function App() {
   const [note, setNote] = useState<NoteData | null>(null)
@@ -34,26 +59,27 @@ function App() {
 
   return (
     <div className={`bg-crust text-3xl w-screen h-screen flex flex-col ${rounded}`}>
-      <div className="flex text-text p-1 gap-3">
-        <div data-tauri-drag-region className="grow" />
-        <Minimize2 onClick={() => appWindow.minimize()} size={16} />
-        <Maximize onClick={() => {
-          setIsFullScreen(!isFullScreen);
-          appWindow.toggleMaximize()
-          void checkIsFullScreen()
-        }} size={16} />
-        <X onClick={() => appWindow.close()} size={16} />
-      </div>
-      <div className="flex flex-row justify-between flex-1 min-h-0">
-        <div className="flex flex-col">
-          <Search className="m-2 text-subtext1" size={24} />
-          <div className="grow" />
-          <Settings className="m-2 text-subtext1" size={24} />
-        </div>
-        <div className="bg-base w-full flex flex-row rounded-tl-md min-h-0">
-          {content}
-        </div>
-      </div>
+    <div className="flex text-text p-1 gap-3">
+    <div data-tauri-drag-region className="grow" />
+    <Minimize2 onClick={() => appWindow.minimize()} size={16} />
+    <Maximize onClick={() => {
+      setIsFullScreen(!isFullScreen);
+      appWindow.toggleMaximize()
+      void checkIsFullScreen()
+    }} size={16} />
+    <X onClick={() => appWindow.close()} size={16} />
+    </div>
+    <div className="flex flex-row justify-between flex-1 min-h-0">
+    <div className="flex flex-col">
+    <SideButton icon={Search} tooltip="Search" />
+    <SideButton icon={FilePlus2} tooltip="New Note"/>
+    <div className="grow" />
+    <SideButton icon={Settings} tooltip="Settings"/>
+    </div>
+    <div className="bg-base w-full flex flex-row rounded-tl-md min-h-0">
+    {content}
+    </div>
+    </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        prose: ["Montserrat", "sans-serif"]
-     }
-    },
+  	extend: {
+  		fontFamily: {
+  			prose: ["Montserrat", "sans-serif"]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
   plugins: [require("@catppuccin/tailwindcss")({
     prefix: false,
     defaultFlavour: "mocha",
-  })],
+  }),
+      require("tailwindcss-animate")
+],
 }
 
