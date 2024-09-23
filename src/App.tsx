5cc876d3b0b9190f2682cb7193437d80c5eb835e
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-23 06:28:02 -0700

    better lint setup

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ import { getCurrentWindow } from '@tauri-apps/api/window'
 
 import { NavButton } from './components/ui/nav-button'
 
-function App (): JSX.Element {
+function App(): JSX.Element {
   const [note, setNote] = useState<NoteData | null>(null)
 
   useEffect(() => {
-    async function loadLatest (): Promise<void> {
+    async function loadLatest(): Promise<void> {
       const result = await getLastUpdated()
       setNote(result)
       const all = await getAllNotes()
@@ -27,8 +27,7 @@ function App (): JSX.Element {
     loadLatest()
   }, [])
 
-  const content =
-    note !== null ? <Editor note={note} /> : <div>Loading...</div>
+  const content = note !== null ? <Editor note={note} /> : <div>Loading...</div>
   const appWindow = getCurrentWindow()
 
   const [isFullScreen, setIsFullScreen] = useState(false)
@@ -48,9 +47,14 @@ function App (): JSX.Element {
     <div
       className={`bg-crust text-3xl w-screen h-screen flex flex-col ${rounded}`}
     >
-      <div className='flex text-text p-1 gap-3'>
-        <div data-tauri-drag-region className='grow' />
-        <Minimize2 onClick={() => { appWindow.minimize() }} size={16} />
+      <div className="flex text-text p-1 gap-3">
+        <div data-tauri-drag-region className="grow" />
+        <Minimize2
+          onClick={() => {
+            appWindow.minimize()
+          }}
+          size={16}
+        />
         <Maximize
           onClick={() => {
             setIsFullScreen(!isFullScreen)
@@ -59,16 +63,27 @@ function App (): JSX.Element {
           }}
           size={16}
         />
-        <X onClick={() => { appWindow.close() }} size={16} />
+        <X
+          onClick={() => {
+            appWindow.close()
+          }}
+          size={16}
+        />
       </div>
-      <div className='flex flex-row justify-between flex-1 min-h-0'>
-        <div className='flex flex-col'>
-          <NavButton icon={Search} tooltip='Search' />
-          <NavButton onClick={() => { createNote('Unnamed Note') }} icon={FilePlus2} tooltip='New Note' />
-          <div className='grow' />
-          <NavButton icon={Settings} tooltip='Settings' />
+      <div className="flex flex-row justify-between flex-1 min-h-0">
+        <div className="flex flex-col">
+          <NavButton icon={Search} tooltip="Search" />
+          <NavButton
+            onClick={() => {
+              createNote('Unnamed Note')
+            }}
+            icon={FilePlus2}
+            tooltip="New Note"
+          />
+          <div className="grow" />
+          <NavButton icon={Settings} tooltip="Settings" />
         </div>
-        <div className='bg-base w-full flex flex-row rounded-tl-md min-h-0'>
+        <div className="bg-base w-full flex flex-row rounded-tl-md min-h-0">
           {content}
         </div>
       </div>
