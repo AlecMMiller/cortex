e9ebfcc3d1923b7486c369a64122e99698dd6c1d
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-23 07:43:34 -0700

    move window wrapper to own file

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from 'react'
 import { getAllNotes, getLastUpdated } from './commands/note'
 import Editor from './editor'
 import { NoteData } from './types'
-import { getCurrentWindow } from '@tauri-apps/api/window'
-import { Sidebar } from './components/widgets/Sidebar'
-import { Topbar } from './components/widgets/Topbar'
+import { WindowWrapper } from './components/widgets/WindowWrapper'
 
 function App(): JSX.Element {
   const [note, setNote] = useState<NoteData | null>(null)
@@ -20,44 +18,8 @@ function App(): JSX.Element {
   }, [])
 
   const content = note !== null ? <Editor note={note} /> : <div>Loading...</div>
-  const appWindow = getCurrentWindow()
 
-  const [isFullScreen, setIsFullScreen] = useState(false)
-
-  const checkIsFullScreen = async (): Promise<void> => {
-    const state = await appWindow.isMaximized()
-    setIsFullScreen(state)
-  }
-
-  useEffect(() => {
-    checkIsFullScreen()
-  }, [appWindow])
-
-  const rounded = isFullScreen ? '' : 'rounded-md'
-
-  return (
-    <div
-      className={`bg-crust text-3xl w-screen h-screen flex flex-col ${rounded}`}
-    >
-      <Topbar
-        close={() => appWindow.close()}
-        toggleMaximize={() => {
-          setIsFullScreen(!isFullScreen)
-          appWindow.toggleMaximize()
-          checkIsFullScreen()
-        }}
-        minimize={() => {
-          appWindow.minimize()
-        }}
-      />
-      <div className="flex flex-row justify-between flex-1 min-h-0">
-        <Sidebar />
-        <div className="bg-base w-full flex flex-row rounded-tl-md min-h-0">
-          {content}
-        </div>
-      </div>
-    </div>
-  )
+  return <WindowWrapper>{content}</WindowWrapper>
 }
 
 export default App
---
+++ src/components/widgets/WindowWrapper.tsx
@@ -0,0 +1,49 @@
+import { getCurrentWindow } from '@tauri-apps/api/window'
+import { useState, useEffect } from 'react'
+import { Sidebar } from './Sidebar'
+import { Topbar } from './Topbar'
+
+interface WindowWrapperProps {
+  children: React.ReactNode
+}
+
+export function WindowWrapper(props: WindowWrapperProps): JSX.Element {
+  const appWindow = getCurrentWindow()
+
+  const [isFullScreen, setIsFullScreen] = useState(false)
+
+  const checkIsFullScreen = async (): Promise<void> => {
+    const state = await appWindow.isMaximized()
+    setIsFullScreen(state)
+  }
+
+  useEffect(() => {
+    checkIsFullScreen()
+  }, [appWindow])
+
+  const rounded = isFullScreen ? '' : 'rounded-md'
+
+  return (
+    <div
+      className={`bg-crust text-3xl w-screen h-screen flex flex-col ${rounded}`}
+    >
+      <Topbar
+        close={() => appWindow.close()}
+        toggleMaximize={() => {
+          setIsFullScreen(!isFullScreen)
+          appWindow.toggleMaximize()
+          checkIsFullScreen()
+        }}
+        minimize={() => {
+          appWindow.minimize()
+        }}
+      />
+      <div className="flex flex-row justify-between flex-1 min-h-0">
+        <Sidebar />
+        <div className="bg-base w-full flex flex-row rounded-tl-md min-h-0">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
