4856057611c6cb0626dc562c248d6a59564b6772
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-23 08:02:36 -0700

    correct handler for minimize

--- src/components/widgets/Topbar.tsx
+++ src/components/widgets/Topbar.tsx
@@ -20,7 +20,7 @@ export function Topbar(props: TopbarProps): JSX.Element {
   return (
     <div className="flex text-text p-1 gap-3">
       <div data-tauri-drag-region className="grow" />
-      <TopButton onClick={props.handleClose} icon={Minimize2} />
+      <TopButton onClick={props.handleMinimize} icon={Minimize2} />
       <TopButton onClick={props.handleMaximize} icon={Maximize} />
       <TopButton onClick={props.handleClose} icon={X} />
     </div>
