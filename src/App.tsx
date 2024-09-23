d44aa38d0a7ce7a13f59c2b7944ca823417becb0
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-23 07:53:33 -0700

    use app router

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import { useEffect, useState } from 'react'
-import { getAllNotes, getLastUpdated } from './commands/note'
-import Editor from './editor'
-import { NoteData } from './types'
 import { WindowWrapper } from './components/widgets/WindowWrapper'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { TestPage } from './routes/Test'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <TestPage />,
+  },
+])
 
 function App(): JSX.Element {
-  const [note, setNote] = useState<NoteData | null>(null)
-
-  useEffect(() => {
-    async function loadLatest(): Promise<void> {
-      const result = await getLastUpdated()
-      setNote(result)
-      const all = await getAllNotes()
-      console.log(all)
-    }
-    loadLatest()
-  }, [])
-
-  const content = note !== null ? <Editor note={note} /> : <div>Loading...</div>
-
-  return <WindowWrapper>{content}</WindowWrapper>
+  return (
+    <WindowWrapper>
+      <RouterProvider router={router} />
+    </WindowWrapper>
+  )
 }
 
 export default App
