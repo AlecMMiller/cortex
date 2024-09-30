c8843419ff874166f09b0303cae4aebd10895ff7
Alec Miller <alecmichaelmiller@gmail.com>
2024-09-29 18:11:04 -0700

    refactor link to common logic

--- src/plugins/ExternalLink.tsx
+++ src/plugins/ExternalLink.tsx
@@ -1,91 +1,13 @@
-/**
- * Originally derived from LexicalClickableLinkPlugin
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-import { $isLinkNode } from '@lexical/link'
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import { $findMatchingParent } from '@lexical/utils'
-import {
-  $getNearestNodeFromDOMNode,
-  $getSelection,
-  $isElementNode,
-  $isRangeSelection,
-  getNearestEditorFromDOMNode,
-} from 'lexical'
-import { useEffect } from 'react'
-
+import { $isLinkNode, LinkNode } from '@lexical/link'
 import { open } from '@tauri-apps/plugin-shell'
+import { makeLinkPlugin } from './LinkCommon'
 
-export function ExternalLinkPlugin({
-  newTab = true,
-  disabled = false,
-}: {
-  newTab?: boolean
-  disabled?: boolean
-}): null {
-  const [editor] = useLexicalComposerContext()
-
-  useEffect(() => {
-    const onClick = (event: MouseEvent) => {
-      const target = event.target
-      if (!(target instanceof Node)) {
-        return
-      }
-      const nearestEditor = getNearestEditorFromDOMNode(target)
-
-      if (nearestEditor === null) {
-        return
-      }
-
-      let url = null
-      nearestEditor.update(() => {
-        const clickedNode = $getNearestNodeFromDOMNode(target)
-        if (clickedNode !== null) {
-          const maybeLinkNode = $findMatchingParent(clickedNode, $isElementNode)
-          if (!disabled) {
-            if ($isLinkNode(maybeLinkNode)) {
-              url = maybeLinkNode.sanitizeUrl(maybeLinkNode.getURL())
-            }
-          }
-        }
-      })
-
-      if (url === null || url === '') {
-        return
-      }
-
-      // Allow user to select link text without follwing url
-      const selection = editor.getEditorState().read($getSelection)
-      if ($isRangeSelection(selection) && !selection.isCollapsed()) {
-        event.preventDefault()
-        return
-      }
-      open(url)
-      event.preventDefault()
-    }
-
-    const onMouseUp = (event: MouseEvent) => {
-      if (event.button === 1) {
-        onClick(event)
-      }
-    }
-
-    return editor.registerRootListener((rootElement, prevRootElement) => {
-      if (prevRootElement !== null) {
-        prevRootElement.removeEventListener('click', onClick)
-        prevRootElement.removeEventListener('mouseup', onMouseUp)
-      }
-      if (rootElement !== null) {
-        rootElement.addEventListener('click', onClick)
-        rootElement.addEventListener('mouseup', onMouseUp)
-      }
-    })
-  }, [editor, newTab, disabled])
-
-  return null
-}
+export const ExternalLinkPlugin = makeLinkPlugin<LinkNode>(
+  $isLinkNode,
+  (node: LinkNode) => {
+    return node.sanitizeUrl(node.getURL())
+  },
+  (url: string) => {
+    open(url)
+  },
+)
--- src/plugins/InternalLink.tsx
+++ src/plugins/InternalLink.tsx
@@ -1,92 +1,17 @@
-/**
- * Originally derived from LexicalClickableLinkPlugin
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
-
-import { $isInternalLinkNode } from '@/nodes/InternalLink'
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
-import { $findMatchingParent } from '@lexical/utils'
+import { $isInternalLinkNode, InternalLinkNode } from '@/nodes/InternalLink'
 import { useNavigate } from '@tanstack/react-router'
-import {
-  $getNearestNodeFromDOMNode,
-  $getSelection,
-  $isElementNode,
-  $isRangeSelection,
-  getNearestEditorFromDOMNode,
-} from 'lexical'
-import { useEffect } from 'react'
+import { makeLinkPlugin } from './LinkCommon'
 
-export function InternalLinkPlugin({
-  newTab = true,
-  disabled = false,
-}: {
-  newTab?: boolean
-  disabled?: boolean
-}): null {
-  const [editor] = useLexicalComposerContext()
-  const navigation = useNavigate()
-
-  useEffect(() => {
-    const onClick = (event: MouseEvent) => {
-      const target = event.target
-      if (!(target instanceof Node)) {
-        return
-      }
-      const nearestEditor = getNearestEditorFromDOMNode(target)
-
-      if (nearestEditor === null) {
-        return
-      }
-
-      let url = null
-      nearestEditor.update(() => {
-        const clickedNode = $getNearestNodeFromDOMNode(target)
-        if (clickedNode !== null) {
-          const maybeLinkNode = $findMatchingParent(clickedNode, $isElementNode)
-          if (!disabled) {
-            if ($isInternalLinkNode(maybeLinkNode)) {
-              url = maybeLinkNode.getURL()
-            }
-          }
-        }
-      })
-
-      if (url === null || url === '') {
-        return
-      }
-
-      // Allow user to select link text without follwing url
-      const selection = editor.getEditorState().read($getSelection)
-      if ($isRangeSelection(selection) && !selection.isCollapsed()) {
-        event.preventDefault()
-        return
-      }
-
-      navigation({ to: url })
-      event.preventDefault()
-    }
-
-    const onMouseUp = (event: MouseEvent) => {
-      if (event.button === 1) {
-        onClick(event)
-      }
-    }
-
-    return editor.registerRootListener((rootElement, prevRootElement) => {
-      if (prevRootElement !== null) {
-        prevRootElement.removeEventListener('click', onClick)
-        prevRootElement.removeEventListener('mouseup', onMouseUp)
-      }
-      if (rootElement !== null) {
-        rootElement.addEventListener('click', onClick)
-        rootElement.addEventListener('mouseup', onMouseUp)
-      }
-    })
-  }, [editor, newTab, disabled])
-
-  return null
+export function InternalLinkPlugin() {
+  const navigate = useNavigate()
+  const Nested = makeLinkPlugin<InternalLinkNode>(
+    $isInternalLinkNode,
+    (node: InternalLinkNode) => {
+      return node.getURL()
+    },
+    (url: string) => {
+      navigate({ to: url })
+    },
+  )
+  return <Nested />
 }
