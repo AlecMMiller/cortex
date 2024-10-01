5419a1c990bc59471ba39425ac76d3ed588fb4e9
Alec Miller <alecmichaelmiller@gmail.com>
2024-10-01 11:56:55 -0700

    move i18n to own folder

---
+++ src/i18n/en.ts
@@ -0,0 +1,12 @@
+export const en = {
+  translation: {
+    Home: 'Home',
+    Search: 'Search',
+    'New Note': 'New Note',
+    Settings: 'Settings',
+    Title: 'Title',
+    'Note Title': 'Note Title',
+    Create: 'Create',
+    'Recent Notes': 'Recent Notes',
+  },
+}
