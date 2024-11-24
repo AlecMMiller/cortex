3bb2f3752a59d32c01acb05477a90a754be48d1e
Alec Miller <alecmichaelmiller@gmail.com>
2024-11-24 05:57:01 -0700

    tweaks

--- flake.nix
+++ flake.nix
@@ -26,7 +26,22 @@
         {
           devShells.default = pkgs.mkShell {
 
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              gobject-introspection
+              cargo
+              cargo-tauri
+              nodejs
+            ];
+
             buildInputs = with pkgs; [
+              at-spi2-atk
+              atkmm
+              cairo
+              gdk-pixbuf
+              harfbuzz
+              librsvg
+              pango
               libiconv
 
               # Rust
