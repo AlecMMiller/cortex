584ea20804ca0539cacfd4739f0fa57eaedfbeb3
Alec Miller <alecmichaelmiller@gmail.com>
2024-12-01 07:54:24 -0700

    locale switcher in storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 import type { Preview } from '@storybook/react'
 import '../src/styles.css'
 import {
@@ -21,7 +21,13 @@ const withI18next = (Story) => {
 
 const preview: Preview = {
   decorators: [
-    (Story) => {
+    (Story, context) => {
+      const { locale } = context.globals
+
+      useEffect(() => {
+        i18n.changeLanguage(locale)
+      }, [locale])
+
       return (
         <I18nextProvider i18n={i18n}>
           <RouterProvider
@@ -46,6 +52,21 @@ const preview: Preview = {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+  },
+}
+
+export const globalTypes = {
+  locale: {
+    name: 'Locale',
+    description: 'Internationalization locale',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', title: 'English' },
+        { value: 'ja', title: '日本語' },
+      ],
+      showName: true,
     },
   },
 }
