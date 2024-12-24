f13dd4fcdd286b69b7c818bdf9eb9dda92b9d1d5
Alec Miller <alecmichaelmiller@gmail.com>
2024-12-23 20:39:22 -0700

    break out responsemap

--- src-tauri/src/database/entity.rs
+++ src-tauri/src/database/entity.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 use rusqlite::{params, Error, Result, Transaction};
 use serde::Deserialize;
@@ -11,6 +11,7 @@ use super::{
     attribute_schema::{AttributeSchemaId, Quantity, RawAttributeSchema, SchemaMap},
     attribute_type::{AttributeType, SimpleAttributeType},
     entity_schema::EntitySchemaId,
+    response_map::{EntitiesData, ResponseMap},
 };
 
 create_id!(EntityId);
@@ -78,82 +79,6 @@ impl<'a> IndividualRequests<'a> {
 
 struct RequestPlan<'a> {
     text: IndividualRequests<'a>,
-}
-
-struct ResponseMap {
-    current_entity: EntityId,
-    current_attribute: AttributeSchemaId,
-    current_vector: Vec<Value>,
-    entities: EntitiesData,
-}
-
-type EntityData = HashMap<AttributeSchemaId, Vec<Value>>;
-type EntitiesData = HashMap<EntityId, EntityData>;
-
-impl ResponseMap {
-    fn new(first_entity: EntityId, first_attribute: AttributeSchemaId, first_value: Value) -> Self {
-        let mut vector = Vec::new();
-        let entities: EntitiesData = HashMap::new();
-
-        vector.push(first_value);
-
-        Self {
-            current_entity: first_entity,
-            current_attribute: first_attribute,
-            current_vector: vector,
-            entities,
-        }
-    }
-
-    fn push_current(mut self) -> Self {
-        let entity = self
-            .entities
-            .entry(self.current_entity.clone())
-            .or_insert(HashMap::new());
-
-        entity.insert(self.current_attribute.clone(), self.current_vector);
-        self.current_vector = Vec::new();
-        self
-    }
-
-    fn change_attribute(mut self, attribute: AttributeSchemaId) -> Self {
-        self = self.push_current();
-        self.current_attribute = attribute;
-        self
-    }
-
-    fn change_entity(mut self, entity: EntityId, attribute: AttributeSchemaId) -> Self {
-        self = self.change_attribute(attribute);
-        self.current_entity = entity;
-        self
-    }
-
-    fn add_value(mut self, entity: EntityId, attribute: AttributeSchemaId, value: Value) -> Self {
-        if entity != self.current_entity {
-            self = self.change_entity(entity, attribute)
-        } else if attribute != self.current_attribute {
-            self = self.change_attribute(attribute);
-        }
-
-        self.current_vector.push(value);
-        self
-    }
-
-    pub fn finalize(self) -> EntitiesData {
-        self.push_current().entities
-    }
-
-    pub fn add(
-        builder: Option<Self>,
-        entity: EntityId,
-        attribute: AttributeSchemaId,
-        value: Value,
-    ) -> Option<Self> {
-        match builder {
-            Some(b) => Some(b.add_value(entity, attribute, value)),
-            None => Some(Self::new(entity, attribute, value)),
-        }
-    }
 }
 
 impl<'a> RequestPlan<'a> {
--- src-tauri/src/database/mod.rs
+++ src-tauri/src/database/mod.rs
@@ -3,4 +3,5 @@ pub mod attribute_type;
 pub mod entity;
 pub mod entity_schema;
 mod migration;
+mod response_map;
 mod test;
